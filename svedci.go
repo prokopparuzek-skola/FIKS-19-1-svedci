c129dc61da49efbdb58305dfc56f6044f03d77ab
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-31 17:52:48 +0100

    funguje na priklady v zadani

--- svedci.go
+++ svedci.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
+func split(friends [][]int) (ans []int) {
+	ans = make([]int, len(friends)-1)
+	var d1, d2 map[int]bool
+	d1 = make(map[int]bool)
+	d2 = make(map[int]bool)
+
+	for i := 1; i < len(friends); i++ {
+		d1[i] = true
+	}
+
+	for i := 1; i < len(friends); i++ {
+		f := friends[i]
+		var cnt int
+		for _, neigh := range f {
+			if d1[neigh] == true {
+				cnt++
+			}
+		}
+		if cnt > len(f)/2 {
+			d2[i] = true
+			d1[i] = false
+		}
+	}
+	for i := 0; i < len(ans); i++ {
+		t := i + 1
+		if d1[t] == true {
+			ans[i] = 1
+		} else {
+			ans[i] = 2
+		}
+	}
+	return
+}
+
 func main() {
 	var T int
 	fmt.Scanf("%d", &T)
@@ -14,11 +48,16 @@ func main() {
 		for j := 1; j <= N; j++ {
 			friends[j] = make([]int, 0)
 		}
-		for j := 0; j < N; j++ {
+		for j := 0; j < M; j++ {
 			var from, to int
 			fmt.Scanf("%d%d", &from, &to)
 			friends[from] = append(friends[from], to)
 			friends[to] = append(friends[to], from)
 		}
+		ans := split(friends)
+		for j := 0; j < len(ans)-1; j++ {
+			fmt.Printf("%d ", ans[j])
+		}
+		fmt.Printf("%d\n", ans[len(ans)-1])
 	}
 }
